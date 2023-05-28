632ef41d8af1b0f5aabffd4e834f9f9d294e982b
Рома <rkhabibovrrr@mail.ru>
2023-05-28 22:42:13 +0300

    Исправлены замечания

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $categories = [
     "other" => "Разное"
 ];
 
-$catalog = [
+$catalogs = [
     [
         "title" => "2014 Rossignol District Snowboard",
         "category" => $categories["boards"],
@@ -105,8 +105,8 @@ $catalog = [
 
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($categories as $indexCategory => $categoryName): ?>
-            <li class="promo__item promo__item--<?=$categoryName;?>">
+            <?php foreach ($categories as $сategory => $categoryName): ?>
+            <li class="promo__item promo__item--<?=$сategory;?>">
                 <a class="promo__link" href="pages/all-lots.html"><?=$categoryName;?></a>
             </li>
             <?php endforeach; ?>
@@ -118,18 +118,18 @@ $catalog = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($catalog as $indexCategory => $categoryName): ?>
+            <?php foreach ($catalogs as $catalog => $catalogName): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$categoryName["image"];?>" width="350" height="260" alt="">
+                    <img src="<?=$catalogName["image"];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$categoryName["category"];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$categoryName["title"];?></a></h3>
+                    <span class="lot__category"><?=$catalogName["category"];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$catalogName["title"];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$categoryName["price"];?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$catalogName["price"];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
